def7bc8a24e5eb1b0f4fc18c392f0c854bdc9ec9
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-07 01:19:47 +0530

    Update

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -22,8 +22,8 @@ export default function MainPage() {
         <section className='w-full lg:w-1/2'>
           <form>
           <div className="mb-4">
-    <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
-    <input type="date" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500
+    <label htmlFor={date} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+    <input type="date" id={date} name={date} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500
      focus:border-green-500 block w-full p-2.5 dark:bg-gray-700
       dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
        dark:focus:ring-green-500 dark:focus:border-green-500"  required/>
