d7acb028dc1d13c25d384dd337c9caa4a4f9473a
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-06 22:02:30 +0530

    Update recode generotor

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -11,12 +11,22 @@ export default function MainPage() {
         value of your money in different currencies this tool 
         its here to help.
       </p>
-      <div>
-        <section>
+      <div className='mt-5 flex items-center justify-center flex-col'>
+        <section className='w-full lg:w-1/2'>
           <form>
-          <div className="mb-5">
+          <div className="mb-4">
     <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
-    <input type="date" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required/>
+    <input type="date" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500
+     focus:border-green-500 block w-full p-2.5 dark:bg-gray-700
+      dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+       dark:focus:ring-green-500 dark:focus:border-green-500"  required/>
+  </div>
+  <div className="mb-4">
+    <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Source Currency</label>
+    <input type="date" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500
+     focus:border-green-500 block w-full p-2.5 dark:bg-gray-700
+      dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+       dark:focus:ring-green-500 dark:focus:border-green-500"  required/>
   </div>
           </form>
         </section>
