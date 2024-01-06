8bc98e34a16780b3b1ba2933f40421affe412665
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-01-06 22:16:20 +0530

    Update all the form

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -39,6 +39,15 @@ export default function MainPage() {
         <option value="">Select Target currency</option>
        </select>
   </div>
+  <div className="mb-4">
+    <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount in source currency</label>
+    <input type="text" id="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500
+     focus:border-green-500 block w-full p-2.5 dark:bg-gray-700
+      dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+       dark:focus:ring-green-500 dark:focus:border-green-500" placeholder='Amount in source currency' required/>
+  </div>
+        <button className='bg-green-600 hover:bg-green-700 text-white 
+        font-bold py-2 px-4 rounded-md'>Get the target Currency</button>
           </form>
         </section>
       </div>
